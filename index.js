0e2f51cd1c42976de9993d8f4978914c2b507ba2
Chris Nunez <cenunez47@gmail.com>
2019-01-18 18:13:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+const root = "https://api.github.com";
+
 function getRepositories() {
+  const user = document.getElementById("username").value;
+  const userReps = root + '/users/' + user + '/repos';
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/ark47/repos');
+  req.open('GET', userReps);
   req.send();
+  return false;
+}
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoList = '<ul>' + repos.map(repo => {
+    const dataUsername = 'data-username"' + repo.owner.login + '"';
+    const dataRepoName = 'data-repository=' + repo.name + '"';
+    return(`
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`
+          )
+  }).join('') + '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
